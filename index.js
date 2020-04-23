a5c3899ab9b601d7fdd78f6f3faa0cc530f2af88
wntiv-main <60457971+wntiv-main@users.noreply.github.com>
2020-04-24 09:48:45 +1200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,224 @@
+window.addEvent('load', function(){
+	var scene = new THREE.Scene();
+	var game = document.getElementById("game");
+	var focused = false;
+	var closable = true;
+	var player = {
+		inventory:{
+			inventory: [], 
+			armour: [], 
+			hotbar: {
+				_: [], 
+				selected: 0
+			},
+			onchange: function(){}, 
+			addEvent: addEvent
+		}
+	};
+	var settings = {
+		FOV: 45, 
+		renderDistance: 1000,
+		sensitivity: 0, 
+		camera: "First Person", 
+		calcSensitivity: function(){
+			//-99 - 1000
+			return 0.1+0.1*(this.sensitivity/100);
+		}, 
+		fullscreen: false, 
+		pointerlock: true, 
+		onchange: function(){},  
+		addEvent: addEvent
+	};
+	settings.onchange(settings);
+	var mouse = {
+				//left, right, middle, back, forward
+		buttons: [false, false, false, false, false], 
+		wheel: 0, 
+		pointerIds: [], 
+		onchange: function(){},  
+		addEvent: addEvent
+	};
+	var cameras = {"First Person": 'fp', "Third Person 1": 'tp1', "Third Person 2": 'tp2', cameras: ["First Person", "Third Person 1", "Third Person 2"]};
+	var camera = {gyroY: new THREE.Object3D(), gyroX: new THREE.Object3D(), shell: null, fp: null, tp1: null, tp2: null};
+	camera.shell = (function(THREE){
+		var g = new THREE.BoxGeometry(0.5, 2, 0.5);
+		var m = new THREE.MeshBasicMaterial({color: 0x000000, opacity:0, transparent:true});
+		var c = new THREE.Mesh(g, m);
+		return c;
+	})(THREE);
+	camera.fp = new THREE.PerspectiveCamera(settings.FOV, window.innerWidth/window.innerHeight, 0.1, settings.renderDistance);
+	camera.fp.position.z = 0.4;
+	camera.gyroX.add(camera.fp);
+
+	camera.tp1 = new THREE.PerspectiveCamera(settings.FOV, window.innerWidth/window.innerHeight, 0.1, settings.renderDistance);
+	camera.tp1.position.z = 10;
+	camera.gyroX.add(camera.tp1);
+	
+	camera.tp2 = new THREE.PerspectiveCamera(settings.FOV, window.innerWidth/window.innerHeight, 0.1, settings.renderDistance);
+	camera.tp2.position.z = -10;
+	camera.tp2.rotation.y = Math.PI;
+	camera.gyroX.add(camera.tp2);
+
+	camera.gyroY.add(camera.gyroX);
+	camera.shell.add(camera.gyroY);
+	scene.add(camera.shell);
+	var renderer = new THREE.WebGLRenderer(), rdE = renderer.domElement;
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	game.appendChild(rdE);
+	window.addEvent('resize', function(){
+		camera.fp.aspect = window.innerWidth/window.innerHeight;
+		camera.fp.updateProjectionMatrix();
+		camera.tp1.aspect = window.innerWidth/window.innerHeight;
+		camera.tp1.updateProjectionMatrix();
+		camera.tp2.aspect = window.innerWidth/window.innerHeight;
+		camera.tp2.updateProjectionMatrix();
+		renderer.setSize(window.innerWidth, window.innerHeight);
+	});
+	rdE.addEvent('click', function(){
+		if(!focused){
+			if(settings.fullscreen){
+				if(game.requestFullscreen)game.requestFullscreen();
+				if(game.requestFullScreen)game.requestFullScreen();
+				if(game.webkitRequestFullscreen)game.webkitRequestFullscreen();
+				if(game.webkitRequestFullScreen)game.webkitRequestFullScreen();
+			}
+			if(settings.pointerlock){
+				if(rdE.requestPointerLock)rdE.requestPointerLock();
+				if(rdE.mozRequestPointerLock)rdE.mozRequestPointerLock();
+			}
+		}
+	});
+	rdE.addEvent('pointerdown', function(e){
+		if(!settings.pointerlock){
+			focused = true;
+			rdE.setPointerCapture(e.pointerId);
+		}
+		if(!mouse.pointerIds.includes(e.pointerId)){
+			mouse.pointerIds.push(e.pointerId);
+			mouse.onchange({type:"pointeradded", detail:mouse});
+		}
+	});
+	rdE.addEvent('mousedown', function(e){
+		mouse.onchange({type:"mousedown", detail:mouse});
+		var buttonsPressed = [0, 0, 0, 0, 0];
+		if(e.hasProperty('buttons')){
+			var buttonIndicies = [1, 2, 4, 8, 16];
+			var binButtons = e.buttons;
+			for(var i=16;i>0;i/=2){
+				if(binButtons>=i){
+					binButtons-=i;
+					buttonsPressed[buttonIndicies.indexOf(i)] = 1;
+				}
+			}
+		}
+		if(e.hasProperty('button')){
+			var buttonIndicies = [0, 2, 1, 3, 4];
+			buttonsPressed[buttonIndicies.indexOf(e.button)] = 1;
+		}else if(e.hasProperty('which')){
+			var buttonIndicies = [1, 3, 2, null, null, 0];
+			buttonsPressed[buttonIndicies.indexOf(e.button)] = 1;
+		}
+		for(var i=0;i<buttonsPressed.length;i++){
+			if(buttonsPressed[i])mouse.buttons[i] = true;
+		}
+	});
+	rdE.addEvent('pointerup', function(e){
+		if(!settings.pointerlock){
+			focused = false;
+			rdE.releasePointerCapture(e.pointerId);
+		}
+	});
+	window.addEvent('mouseup', function(e){
+		mouse.onchange({type:"mouseup", detail:mouse});
+		var buttonsPressed = [0, 0, 0, 0, 0];
+		if(e.hasProperty('buttons')){
+			var buttonIndicies = [1, 2, 4, 8, 16];
+			var binButtons = e.buttons;
+			for(var i=16;i>0;i/=2){
+				if(binButtons>=i){
+					binButtons-=i;
+					buttonsPressed[buttonIndicies.indexOf(i)] = 1;
+				}
+			}
+			mouse.buttons = [false, false, false, false, false];
+			for(var i=0;i<buttonsPressed.length;i++){
+				if(buttonsPressed[i])mouse.buttons[i] = true;
+			}
+		}else{
+			if(e.hasProperty('button')){
+				var buttonIndicies = [0, 2, 1, 3, 4];
+				buttonsPressed[buttonIndicies.indexOf(e.button)] = 1;
+			}else if(e.hasProperty('which')){
+				var buttonIndicies = [1, 3, 2, null, null, 0];
+				buttonsPressed[buttonIndicies.indexOf(e.button)] = 1;
+			}
+			for(var i=0;i<buttonsPressed.length;i++){
+				if(buttonsPressed[i])mouse.buttons[i] = false;
+			}
+		}
+	});
+	document.addEvent('pointerlockchange', function(){focused = !focused;});
+	document.addEvent('pointerlockerror', function(){settings.pointerlock = false;settings.onchange(settings);});
+	document.addEvent('fullscreenerror', function(){settings.fullscreen = false;settings.onchange(settings);});
+	document.addEvent('wheel', function(e){if(mouse.buttons[2]&&Math.abs(e.deltaY/100)>2)return;mouse.wheel+=e.deltaY/100;mouse.onchange({type:"wheelchange", detail:mouse, delta: e.deltaY/100});}); 
+	document.addEvent('contextmenu', 'preventDefault');
+	document.addEventListener('keydown', function(e){e=e||event;if(focused)e.preventDefault();});
+	document.addEventListener('mousedown', function(e){e=e||event;if(focused)e.preventDefault();});
+	window.addEvent('beforeunload', ()=>{if(!closable||focused)return 0;});
+	window.addEvent('keyup', function(e){
+		if(e.code=="F5"||e.key=="F5"||e.charCode==116||e.keyCode==116||e.which==116){
+			settings.camera = cameras.cameras[(cameras.cameras.indexOf(settings.camera) + 1)%cameras.cameras.length];
+			settings.onchange(settings);
+		}
+	});
+	mouse.addEvent('change', function(e){
+		if(e.type=='wheelchange'){
+			player.inventory.hotbar.selected += e.delta;
+			player.inventory.hotbar.selected = Math.floor(player.inventory.hotbar.selected);
+			player.inventory.hotbar.selected %= 7;
+			while(player.inventory.hotbar.selected<0)player.inventory.hotbar.selected+=7;
+		}
+	});
+	settings.addEvent('change', function(e){
+		camera.shell.material.transparent = e.camera=='First Person';
+		if(!e.pointerlock)document.exitPointerLock();
+		if(!e.fullscreen)document.exitFullscreen();
+		camera.fp.fov = e.FOV;
+		camera.fp.far = e.renderDistance;
+		camera.fp.updateProjectionMatrix();
+		camera.tp1.fov = e.FOV;
+		camera.tp1.far = e.renderDistance;
+		camera.tp1.updateProjectionMatrix();
+		camera.tp2.fov = e.FOV;
+		camera.tp2.far = e.renderDistance;
+		camera.tp2.updateProjectionMatrix();
+	});
+	rdE.addEvent('mousemove', function(e){
+		if(!focused)return;
+		var r1 = (!!settings.pointerlock?-1:1), r2 = (settings.camera=='Third Person 2'?-1:1), r3 = (settings.pointerlock||settings.camera=='First Person'?1:-1);
+		camera.gyroY.rotation.y += e.movementX*settings.calcSensitivity()/180*Math.PI*r1*r3;
+		camera.gyroX.rotation.x = Math.min(Math.max(camera.gyroX.rotation.x + e.movementY*settings.calcSensitivity()/180*Math.PI*r1*r2*r3, -Math.PI/2), Math.PI/2);
+	});
+	(function THREEAnimationLoop(){
+		renderer.render(scene, camera[cameras[settings.camera]]);
+		requestAnimationFrame(THREEAnimationLoop);
+	})();
+
+	var geometry = new THREE.BoxGeometry(1, 1, 1);
+	var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+	var cube = new THREE.Mesh( geometry, material );
+	cube.position.z = -5;
+	scene.add(cube);
+
+	var geometry = new THREE.BoxGeometry(1, 1, 1);
+	var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+	var cube = new THREE.Mesh( geometry, material );
+	cube.position.y = -5;
+	scene.add(cube);
+
+	expose(settings).as('settings');
+	expose(mouse).as('mouse.info');
+	expose(scene).as('THREE.myScene');
+	expose(camera.shell).as('camera.shell');
+	expose(player).as('player');
+});
---
+++ libs.js
@@ -0,0 +1,315 @@
+//element.addEvent: add events so that element.on_____(e) is possible
+function addEvent(even_t, callback) {
+	if (!this.events) this.events = {};
+	if (!this.events[even_t]) {
+		this.events[even_t] = [];
+		var element = this;
+		this['on' + even_t] = function (e) {
+			e = e || window.event;
+			if (element.events[even_t + '.preventDefault']) e.preventDefault();
+			var events = element.events[even_t], r = undefined;
+			for (var i = 0; i < events.length; i++) {
+				r = events[i](e);
+			}
+			return r;
+		}
+	}
+	if (typeof callback == 'function' || callback instanceof Function) this.events[even_t].push(callback);
+	else if (callback == 'preventDefault') this.events[even_t + '.preventDefault'] = true;
+}
+HTMLElement.prototype.addEvent = addEvent;
+     Window.prototype.addEvent = addEvent;
+   Document.prototype.addEvent = addEvent;
+
+//Object.hasProperty(name)
+Object.defineProperty(Object.prototype, 'hasProperty', { value: function (name) { return !"undefinednull".includes(typeof this[name]); }, enumerable: false });
+
+//expose a variable to window
+function expose(variable) {
+	return {
+		variable: variable,
+		as: function (name) {
+			path = name.split('.');
+			place = window;
+			for (var i = 0; i < path.length - 1; i++) {
+				if (!place[path[i]]) place[path[i]] = {};
+				place = place[path[i]];
+			}
+			place[path[path.length - 1]] = this.variable;
+		}
+	}
+}
+
+//settings utilities
+function Slider(){}
+function Toggle(){}
+
+
+//noise
+(function () {
+	const PERLIN_YWRAPB = 4;
+	const PERLIN_YWRAP = 1 << PERLIN_YWRAPB;
+	const PERLIN_ZWRAPB = 8;
+	const PERLIN_ZWRAP = 1 << PERLIN_ZWRAPB;
+	const PERLIN_SIZE = 4095;
+	let perlin_octaves = 4;
+	let perlin_amp_falloff = 0.5;
+	const scaled_cosine = i => 0.5 * (1.0 - Math.cos(i * Math.PI));
+	let perlin;
+	function noise (x, y = 0, z = 0) {
+		if (perlin == null) {
+			perlin = new Array(PERLIN_SIZE + 1);
+			for (let i = 0; i < PERLIN_SIZE + 1; i++) {
+				perlin[i] = Math.random();
+			}
+		}
+		if (x < 0) {
+			x = -x;
+		}
+		if (y < 0) {
+			y = -y;
+		}
+		if (z < 0) {
+			z = -z;
+		}
+		let xi = Math.floor(x),
+			yi = Math.floor(y),
+			zi = Math.floor(z);
+		let xf = x - xi;
+		let yf = y - yi;
+		let zf = z - zi;
+		let rxf, ryf;
+		let r = 0;
+		let ampl = 0.5;
+		let n1, n2, n3;
+		for (let o = 0; o < perlin_octaves; o++) {
+			let of = xi + (yi << PERLIN_YWRAPB) + (zi << PERLIN_ZWRAPB);
+			rxf = scaled_cosine(xf);
+			ryf = scaled_cosine(yf);
+			n1 = perlin[of & PERLIN_SIZE];
+			n1 += rxf * (perlin[(of + 1) & PERLIN_SIZE] - n1);
+			n2 = perlin[(of + PERLIN_YWRAP) & PERLIN_SIZE];
+			n2 += rxf * (perlin[(of + PERLIN_YWRAP + 1) & PERLIN_SIZE] - n2);
+			n1 += ryf * (n2 - n1);
+			of += PERLIN_ZWRAP;
+			n2 = perlin[of & PERLIN_SIZE];
+			n2 += rxf * (perlin[(of + 1) & PERLIN_SIZE] - n2);
+			n3 = perlin[(of + PERLIN_YWRAP) & PERLIN_SIZE];
+			n3 += rxf * (perlin[(of + PERLIN_YWRAP + 1) & PERLIN_SIZE] - n3);
+			n2 += ryf * (n3 - n2);
+			n1 += scaled_cosine(zf) * (n2 - n1);
+			r += n1 * ampl;
+			ampl *= perlin_amp_falloff;
+			xi <<= 1;
+			xf *= 2;
+			yi <<= 1;
+			yf *= 2;
+			zi <<= 1;
+			zf *= 2;
+			if (xf >= 1.0) {
+				xi++;
+				xf--;
+			}
+			if (yf >= 1.0) {
+				yi++;
+				yf--;
+			}
+			if (zf >= 1.0) {
+				zi++;
+				zf--;
+			}
+		}
+		return r;
+	};
+	function noiseDetail (lod, falloff) {
+		if (lod > 0) {
+			perlin_octaves = lod;
+		}
+		if (falloff > 0) {
+			perlin_amp_falloff = falloff;
+		}
+	};
+	function noiseSeed (seed) {
+		const lcg = (() => {
+			const m = 4294967296;
+			const a = 1664525;
+			const c = 1013904223;
+			let seed, z;
+			return {
+				setSeed(val) {
+					z = seed = (val == null ? Math.random() * m : val) >>> 0;
+				},
+				getSeed() {
+					return seed;
+				},
+				rand() {
+					z = (a * z + c) % m;
+					return z / m;
+				}
+			};
+		})();
+		lcg.setSeed(seed);
+		perlin = new Array(PERLIN_SIZE + 1);
+		for (let i = 0; i < PERLIN_SIZE + 1; i++) {
+			perlin[i] = lcg.rand();
+		}
+	};
+	expose(noise).as('noise.get');
+	expose(noiseDetail).as('noise.detail');
+	expose(noiseSeed).as('noise.seed');
+})();
+
+//length of arrays with negative numbers
+function length(a){var r=0;for(var i in a){r++}return r;}
+
+//render heightmap
+(function(){
+	var canvas = document.createElement('canvas');
+	var ctx = canvas.getContext('2d');
+	function drawMap(w=100, h=100, src, callback, heightOffset=1, defX=0, defY=0) {//draw a landscape from an image heightmap(pass in image url, (new Image()), or format Object{data:[], width:0, height:0})
+		var depth = h;
+		var width = w;
+		canvas.width = w;
+		canvas.height = h;
+		ctx.clearRect(0, 0, w, h);
+		if (typeof src == "string") {
+			var img = new Image();
+			img.src = src;
+			img.onload = function () {
+				var spacingX = width / img.width;
+				var spacingZ = depth / img.height;
+				ctx.drawImage(img, 0, 0, width, depth);
+				var pixel = ctx.getImageData(0, 0, width, depth);
+				var geom = new THREE.Geometry();
+				var output = [];
+				for (var x = 0; x < depth; x++) {
+					for (var z = 0; z < width; z++) {
+						var yValue = pixel.data[z * 4 + (depth * x * 4)] / heightOffset;
+						var vertex = new THREE.Vector3(x * spacingX, yValue, z * spacingZ);
+						geom.vertices.push(vertex);
+					}
+				}
+				for (var z = 0; z < depth - 1; z++) {
+					for (var x = 0; x < width - 1; x++) {
+						var a = x + z * width;
+						var b = (x + 1) + (z * width);
+						var c = x + ((z + 1) * width);
+						var d = (x + 1) + ((z + 1) * width);
+						var face1 = new THREE.Face3(a, b, d);
+						var face2 = new THREE.Face3(d, c, a);
+						geom.faces.push(face1);
+						geom.faces.push(face2);
+						geom.faces[geom.faces.length - 1].color = new THREE.Color(0xff0000);
+					}
+				}
+				geom.computeVertexNormals(true);
+				geom.computeFaceNormals();
+				var meshI = new THREE.Mesh(geom, new THREE.MeshPhongMaterial({/*color: 0xf00,*/ side: THREE.DoubleSide, flatShading: !1, /*emissive:0x0f0, specular:0x00f,*/ shininess: 1, fog: true, /*envMap:'reflection'*/ }));
+				meshI.position.z = h/2; meshI.position.x = w/2;
+				var mesh = new THREE.Object3D();
+				mesh.position.x = defX; mesh.position.z = defY;
+				mesh.add(meshI);
+				callback(mesh);
+			}
+		} else if ((src instanceof ImageData) || (src['data'] && src['width'] && src['height'])) {
+			var offset = src.data.length / (src.width * src.height);
+			var spacingX = width / src.width;
+			var spacingZ = depth / src.height;
+			var geom = new THREE.Geometry();
+			var pixel = src;
+			var output = [];
+			for (var x = 0; x < src.height; x++) {
+				for (var z = 0; z < src.width; z++) {
+					var yValue = pixel.data[z * offset + (src.height * x * offset)] / heightOffset;
+					var vertex = new THREE.Vector3(x * spacingX, yValue, z * spacingZ);
+					geom.vertices.push(vertex);
+				}
+			}
+			for (var z = 0; z < src.height - 1; z++) {
+				for (var x = 0; x < src.width - 1; x++) {
+					var a = x + z * src.width;
+					var b = (x + 1) + (z * src.width);
+					var c = x + ((z + 1) * src.width);
+					var d = (x + 1) + ((z + 1) * src.width);
+					var face1 = new THREE.Face3(a, b, d);
+					var face2 = new THREE.Face3(d, c, a);
+					geom.faces.push(face1);
+					geom.faces.push(face2);
+					geom.faces[geom.faces.length - 1].color = new THREE.Color(0xff0000);
+				}
+			}
+			geom.computeVertexNormals(true);
+			geom.computeFaceNormals();
+			var meshI = new THREE.Mesh(geom, new THREE.MeshPhongMaterial({color: 0xf00, side: THREE.DoubleSide, flatShading: !1, emissive:0x0f0, specular:0x00f, shininess: 1, fog: true, /*envMap:'reflection'*/ }));
+			var mesh = new THREE.Object3D();
+			mesh.position.x = defX; mesh.position.z = defY;
+			mesh.add(meshI);
+			callback(mesh);
+		} else if (src instanceof Image) {
+			var spacingX = width / src.width;
+			var spacingZ = depth / src.height;
+			ctx.drawImage(src, 0, 0, width, depth);
+			var pixel = ctx.getImageData(0, 0, width, depth);
+			var geom = new THREE.Geometry();
+			var output = [];
+			for (var x = 0; x < depth; x++) {
+				for (var z = 0; z < width; z++) {
+					var yValue = pixel.data[z * 4 + (depth * x * 4)] / heightOffset;
+					var vertex = new THREE.Vector3(x * spacingX, yValue, z * spacingZ);
+					geom.vertices.push(vertex);
+				}
+			}
+			for (var z = 0; z < depth - 1; z++) {
+				for (var x = 0; x < width - 1; x++) {
+					var a = x + z * width;
+					var b = (x + 1) + (z * width);
+					var c = x + ((z + 1) * width);
+					var d = (x + 1) + ((z + 1) * width);
+					var face1 = new THREE.Face3(a, b, d);
+					var face2 = new THREE.Face3(d, c, a);
+					geom.faces.push(face1);
+					geom.faces.push(face2);
+					geom.faces[geom.faces.length - 1].color = new THREE.Color(0xff0000);
+				}
+			}
+			geom.computeVertexNormals(true);
+			geom.computeFaceNormals();
+			var meshI = new THREE.Mesh(geom, new THREE.MeshPhongMaterial({/*color: 0xf00,*/ side: THREE.DoubleSide, flatShading: !1, /*emissive:0x0f0, specular:0x00f,*/ shininess: 1, fog: true, /*envMap:'reflection'*/ }));
+			meshI.position.z = h/2; meshI.position.x = w/2;
+			var mesh = new THREE.Object3D();
+			mesh.position.x = defX; mesh.position.z = defY;
+			mesh.add(meshI);
+			callback(mesh);
+		}
+	}
+	function HeightMap(){
+		this.width = 0;
+		this.height = 0;
+		this.loadedChunks = [];
+		this.data = [];
+	}
+	HeightMap.prototype.extend = function(data, chunk){
+		//data is 2D array, 100*100, convert to 1D
+		var formatted = [];
+		if(data.length==10000){
+			formatted = data;
+		}
+		else if(data.length==100){
+			for(var i=0;i<100;i++){
+				[].push.apply(formatted, data[i]);
+			}
+		}
+		else throw new Error('data is not formatted correctly (must be [100 by 100] or [10000])');
+		if(typeof this.data[chunk.y]=='undefined')this.data[chunk.y] = [];
+		this.data[chunk.y][chunk.x] = formatted;
+		if(typeof this.loadedChunks[chunk.y]=='undefined')this.loadedChunks[chunk.y] = [];
+		this.loadedChunks[chunk.y][chunk.x] = true;
+		this.height = length(this.loadedChunks);
+		this.width = 0;
+		for(var i in this.loadedChunks){if(length(this.loadedChunks[i])>this.width)this.width = length(this.loadedChunks[i]);}
+	};
+	HeightMap.prototype.load = function(chunk, callback){
+		drawMap(10, 10, {data:this.data[chunk.y][chunk.x], width: 100, height:100}, callback, 0.1);
+	};
+	expose(HeightMap).as('RenderingInstance');
+})();
